fdea53a9225cb84cb1bd7ab30283d69e5c9d00ba
firstoliudi <first.l11101990@gmail.com>
2024-03-17 23:46:15 -0700

    Initial Commit

---
+++ TrafficAlert/TrafficAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficAlertApp.swift
+//  TrafficAlert
+//
+//  Created by csuftitan on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
